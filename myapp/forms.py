ab3ec7ba6d5d628715a92e18550ce6a86ce3af52
Moses Bartena Towet <121856163+4ourpixels@users.noreply.github.com>
2023-07-04 18:27:47 +0300

    Photo Form updated Overhaul

--- myapp/forms.py
+++ myapp/forms.py
@@ -2,7 +2,7 @@ from django.forms import ModelForm
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
-from .models import Order, Photo
+from .models import *
 
 from django import forms
 
@@ -81,3 +81,30 @@ class PhotoForm(forms.ModelForm):
             self.fields['popular'].widget.attrs['class'] = 'form-check-input'
             self.fields['shop'].widget.attrs['class'] = 'form-check-input'
             self.fields['digital'].widget.attrs['class'] = 'form-check-input'
+
+
+class BlogForm(forms.ModelForm):
+    class Meta:
+        model = Blog
+        fields = (
+            'title',
+            'summary',
+            'content',
+            'author',
+            'keywords',
+            'image',
+            'youtube',
+            'brand',
+        )
+
+        def __init__(self, *args, **kwagrs):
+            super(BlogForm, self).__init__(*args, **kwagrs)
+
+            self.fields['title'].widget.attrs['class'] = 'form-control'
+            self.fields['summary'].widget.attrs['class'] = 'form-control'
+            self.fields['content'].widget.attrs['class'] = 'form-control'
+            self.fields['author'].widget.attrs['class'] = 'form-control'
+            self.fields['keywords'].widget.attrs['class'] = 'form-control'
+            self.fields['image'].widget.attrs['class'] = 'form-control'
+            self.fields['youtube'].widget.attrs['class'] = 'form-control'
+            self.fields['brand'].widget.attrs['class'] = 'form-control'
